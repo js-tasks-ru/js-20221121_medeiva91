efe211e1a440f4975eb0724e527f84892ec65917
Анастасия Пряхина <a.pryakhina@bftcom.com>
2022-12-20 23:34:29 +0300

    Объекты и массивы, основы тестирования

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,19 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+    if (obj) {
+        const keys = Object.keys(obj);
+        const values = Object.values(obj);
+    
+        let newObj = {};
+    
+        values.forEach((value, index) => {
+            newObj[value] = keys[index];
+        });
+    
+        return newObj;   
+    } else {
+        return obj;
+    }
 
 }
